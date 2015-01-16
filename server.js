d6c15228c4d06d0180a63105e934494b6d5ae81f
Sam Gazitt <gazitt@gmail.com>
2015-01-15 23:16:07 -0800

    typo again

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ http.createServer(function(request, response) {
             fs.readFile(t, 'utf8', function(err, data) {
                 var html = mustache.to_html(data, v);
                 response.writeHead(200);
-                reponse.contentType(request.params.file);
+                response.contentType(request.params.file);
                 response.write(html, "binary");
                 response.end()
                 return;
